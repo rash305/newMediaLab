75e45e88c7f5704a4304b23784b29c3c15741c60
sjoerd <s.dijk.nl@gmail.com>
2020-04-11 15:21:58 +0200

    Whoops, made a fuckup. Some new components weren't committed. This version should be buildable.

---
+++ website/src/app/modules/dictionary/signs/signs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-signs',
+  templateUrl: './signs.component.html',
+  styleUrls: ['./signs.component.css']
+})
+export class SignsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
