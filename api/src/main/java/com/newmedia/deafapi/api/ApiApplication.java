65fca23fa27a5b0520d107707a1cb78d56a86250
Sjoerd <s.dijk.nl@gmail.com>
2020-04-26 21:52:28 +0200

    Stable mongo db connection and loggin

--- api/src/main/java/com/newmedia/deafapi/api/ApiApplication.java
+++ api/src/main/java/com/newmedia/deafapi/api/ApiApplication.java
@@ -1,15 +1,24 @@
 package com.newmedia.deafapi.api;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 //@ComponentScan(basePackages={"com.newmedia.deafapi.api.controllers", "com.newmedia.deafapi.api.services"})
-public class ApiApplication {
+public class ApiApplication implements ApplicationRunner {
+
+    private static final Logger logger = LogManager.getLogger(ApiApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
     }
-
+    @Override
+    public void run(ApplicationArguments applicationArguments) throws Exception {
+        logger.debug("Debugging log enabled");
+    }
 }
--- api/src/main/java/com/newmedia/deafapi/api/controllers/CategoryController.java
+++ api/src/main/java/com/newmedia/deafapi/api/controllers/CategoryController.java
@@ -6,13 +6,16 @@ import com.newmedia.deafapi.api.models.Category;
 import com.newmedia.deafapi.api.models.Sign;
 import com.newmedia.deafapi.api.services.Interfaces.ICategoryService;
 import com.newmedia.deafapi.api.utils.ObjectMapperUtils;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 // Add notation to inform spring boot to create an instance of this class and publish it as a rest controller
 @RestController
