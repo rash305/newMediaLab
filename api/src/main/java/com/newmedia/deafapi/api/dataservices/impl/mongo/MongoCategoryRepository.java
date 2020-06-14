c23538a006ed5123c1c28efb49728d2034c840f9
Sjoerd <s.dijk.nl@gmail.com>
2020-06-14 23:29:25 +0200

    Make categories language specific

--- api/src/main/java/com/newmedia/deafapi/api/dataservices/impl/mongo/MongoCategoryRepository.java
+++ api/src/main/java/com/newmedia/deafapi/api/dataservices/impl/mongo/MongoCategoryRepository.java
@@ -1,8 +1,13 @@
 package com.newmedia.deafapi.api.dataservices.impl.mongo;
 
 import com.newmedia.deafapi.api.dataservices.docModels.DocCategory;
+import com.newmedia.deafapi.api.dataservices.docModels.DocFavoriteSign;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface MongoCategoryRepository extends MongoRepository<DocCategory, String> {
+    List<DocCategory> findByLanguage(String language);
+
     //
 }
